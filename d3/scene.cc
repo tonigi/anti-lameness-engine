eb6fc98eb17203e502008e22f6d975b469e75083
dhilvert <dhilvert@auricle.dyndns.org>
2005-10-09 01:00:00 +0000

    Change nearness 0.02 --> 0.01
    darcs-hash:20051009010015-789c2-88b4f8326a0bf96b211cadc77e777479564ffe78.gz

--- d3/scene.cc
+++ d3/scene.cc
@@ -41,7 +41,7 @@ int scene::mpl_type = 0;
 int scene::mpu_type = 1;
 const char *scene::load_model_name = NULL;
 const char *scene::save_model_name = NULL;
-const ale_real scene::nearness = 0.02;
+const ale_real scene::nearness = 0.01;
 
 scene::space *scene::root_space = NULL;
 std::map<struct scene::space *, scene::spatial_info> scene::spatial_info_map;
