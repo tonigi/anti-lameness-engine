a1c813bd286fbaddb4f1b457aa8798f7d9fb18ed
dhilvert <dhilvert@auricle.dyndns.org>
2006-07-14 10:08:00 +0000

    Update copyright headers related to option processing.
    darcs-hash:20060714100839-789c2-d7c112fc3814b2c18dc9a6db3d5651b76fc0f40f.gz

--- ui/input.h
+++ ui/input.h
@@ -1,5 +1,5 @@
-// Copyright 2004 David Hilvert <dhilvert@auricle.dyndns.org>, 
-//                              <dhilvert@ugcs.caltech.edu>
+// Copyright 2002, 2003, 2004, 2005, 2006 David Hilvert <dhilvert@auricle.dyndns.org>, 
+//                                                      <dhilvert@ugcs.caltech.edu>
 
 /*  This file is part of the Anti-Lamenessing Engine.
 
