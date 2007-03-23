ea117b77ec847c4b6fa86a84b77607f0dd8d2a7d
David Hilvert <dhilvert@auricle.dyndns.org>
2007-03-23 06:13:00 +0000

    d2::trans_multi: Update methods for a trivial multi-transformation implementation.
    darcs-hash:20070323061323-d1b56-d54768e8666af227829369d75decdb08261b625f.gz

--- d2/trans_multi.h
+++ d2/trans_multi.h
@@ -32,7 +32,6 @@
 struct trans_multi : public trans_abstract {
 private:
 	std::stack<trans_single> trans_stack;
-	int _is_projective;
 
 public:	
 
@@ -40,39 +39,31 @@ public:
 	 * Returns non-zero if the transformation might be non-Euclidean.
 	 */
 	int is_projective() {
-		return _is_projective;
+		return trans_stack[0].is_projective();
 	}
 
 	/*
 	 * Projective/Euclidean transformations
 	 */
 	struct point pe(struct point p) const {
+		return trans_stack[0].pe(p);
 	}
 
 	/*
 	 * Inverse transformations
 	 */
 	struct point pei(struct point p) const {
+		return trans_stack[0].pei(p);
 	}
 
-
-#if 0
-	/*
-	 * operator() is the transformation operator.
-	 */
-	struct point operator()(struct point p) {
-		return transform(p);
-	}
-#endif
 
 	/*
 	 * Calculate projective transformation parameters from a euclidean
 	 * transformation.
 	 */
 	void eu_to_gpt() {
-		assert(!_is_projective);
-		assert(0);
-		_is_projective = 1;
+		for (int t = 0; t < trans_stack.size(); t++)
+			trans_stack[t].eu_to_gpt();
 	}
 
 	/*
@@ -80,7 +71,7 @@ public:
 	 */
 	static struct trans_multi eu_identity(const image *i = NULL, ale_pos scale_factor = 1) {
 		struct trans_multi r;
-		assert(0);
+		r.trans_stack.push(trans_single::eu_identity(i, scale_factor));
 		return r;
 	}
 
@@ -89,9 +80,7 @@ public:
 	 */
 	static trans_multi gpt_identity(const image *i, ale_pos scale_factor) {
 		struct trans_multi r = eu_identity(i, scale_factor);
-
 		r.eu_to_gpt();
-
 		return r;
 	}
 
@@ -99,290 +88,102 @@ public:
 	 * Modify a euclidean transform in the indicated manner.
 	 */
 	void eu_modify(int i1, ale_pos diff) {
-
-		assert(!_is_projective);
-
-		assert(0);
-
+		trans_stack.top().eu_modify(i1, diff);
 	}
 
 	/*
 	 * Rotate about a given point in the original reference frame.
 	 */
 	void eu_rotate_about_scaled(point center, ale_pos diff) {
-		assert(0);
-
-		assert(center.defined());
-		point fixpoint = scaled_inverse_transform(center);
-		eu_modify(2, diff);
-		point offset = center - transform_scaled(fixpoint);
-
-		eu_modify(0, offset[0]);
-		eu_modify(1, offset[1]);
+		trans_stack.top().eu_rotate_about_scaled(center, diff);
 	}
 
 	/*
 	 * Modify all euclidean parameters at once.
 	 */
 	void eu_set(ale_pos eu[3]) {
-
-		assert(0);
-		
-		this->eu[0] = eu[0] / scale_factor;
-		this->eu[1] = eu[1] / scale_factor;
-		this->eu[2] = eu[2];
-
-		if (_is_projective) {
-			_is_projective = 0;
-			eu_to_gpt();
-		}
-
+		trans_stack.top().eu_set(eu);
 	}
 
 	/*
 	 * Get the specified euclidean parameter
 	 */
 	ale_pos eu_get(int param) {
-
-		assert(0);
-
-		assert (!_is_projective);
-		assert (param >= 0);
-		assert (param < 3);
-
-		if (param < 2)
-			return eu[param] * scale_factor;
-		else
-			return eu[param];
+		return trans_stack.top().eu_get(param);
 	}
 
 	/*
 	 * Modify a projective transform in the indicated manner.
 	 */
 	void gpt_modify(int i1, int i2, ale_pos diff) {
-
-		assert(0);
-
-		assert (_is_projective);
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
-
-		x[i2][i1] += diff;
-
+		trans_stack.top().gpt_modify(i1, i2, diff);
 	}
 
 	/*
 	 * Modify a projective transform according to the group operation.
 	 */
 	void gr_modify(int i1, int i2, ale_pos diff) {
-
-		assert(0);
-
-		assert (_is_projective);
-		assert (i1 == 0 || i1 == 1);
-
-		point diff_vector = (i1 == 0) 
-			          ? point(diff, 0)
-				  : point(0, diff);
-
-		trans_multi t = *this;
-
-		t.resultant_memo = 0;
-		t.resultant_inverse_memo = 0;
-		t.input_height = (unsigned int) scaled_height();
-		t.input_width  = (unsigned int) scaled_width();
-		t.scale_factor = 1;
-		t.bd_set(0, (ale_pos *) NULL);
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
-
-		x[i2] = t.transform_scaled(t.scaled_inverse_transform(x[i2]) + diff_vector);
+		trans_stack.top().gr_modify(i1, i2, diff);
 	}
 
 	/*
 	 * Modify all projective parameters at once.
 	 */
 	void gpt_set(point x[4]) {
-
-		assert(0);
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
-
-		_is_projective = 1;
-
-		for (int i = 0; i < 4; i++)
-			this->x[i] = x[i];
-
+		trans_stack.top().gpt_set(x);
 	}
 
 	void gpt_set(point x1, point x2, point x3, point x4) {
-
-		assert(0);
-
-		point x[4] = {x1, x2, x3, x4};
-		gpt_set(x);
+		trans_stack.top().gpt_set(x1, x2, x3, x4);
 	}
 
 	/*
 	 * Get the specified projective parameter
 	 */
 	point gpt_get(int point) {
-		assert(0);
-
-		assert (_is_projective);
-		assert (point >= 0);
-		assert (point <  4);
-
-		return x[point];
+		return trans_stack.top().gpt_get(point);
 	}
 
 	/*
 	 * Get the specified projective parameter
 	 */
 	ale_pos gpt_get(int point, int dim) {
-		assert(0);
-
-		assert (_is_projective);
-		assert (dim   >= 0);
-		assert (dim   <  2);
-
-		return gpt_get(point)[dim];
+		return trans_stack.top().gpt_get(point, dim);
 	}
 
 	/*
 	 * Translate by a given amount
 	 */
 	void translate(point p) {
-
-		assert(0);
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
-
-		if  (_is_projective)
-		for (int i = 0; i < 4; i++)
-			x[i]+=p;
-		else {
-			eu[0] += p[0] / scale_factor;
-			eu[1] += p[1] / scale_factor;
-		}
+		trans_stack.top().translate(p);
 	}
 
 	/*
 	 * Rotate by a given amount about a given point.
 	 */
 	void rotate(point p, ale_pos degrees) {
-
-		assert(0);
-
-		ale_pos radians = degrees * M_PI / (double) 180;
-
-		if (_is_projective)
-		for (int i = 0; i <= 4; i++) {
-			x[i] -= p;
-			x[i] = point(
-				x[i][0] * cos(radians) + x[i][1] * sin(radians),
-				x[i][1] * cos(radians) - x[i][0] * sin(radians));
-			x[i] += p;
-		} else {
-			point usi = unscaled_inverse_transform(p);
-
-			eu[2] += degrees;
-			resultant_memo = 0;
-			
-			point p_rot = transform_unscaled(usi);
-
-			eu[0] -= (p_rot[0] - p[0]) / scale_factor;
-			eu[1] -= (p_rot[1] - p[1]) / scale_factor;
-		}
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
+		trans_stack.top().rotate();
 	}
 
 	void reset_memos() {
-		assert(0);
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
-
+		for (int t = 0; t < trans_stack.size(); t++)
+			trans_stack[t].reset_memos();
 	}
 
 	/*
 	 * Rescale a transform with a given factor.
 	 */
 	void specific_rescale(ale_pos factor) {
-		assert(0);
-
-
-		if (_is_projective) {
-
-			for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 2; j++)
-				x[i][j] *= factor;
-
-		} else {
-#if 0
-			/*
-			 * Euclidean scaling is handled in resultant().
-			 */
-			for (int i = 0; i < 2; i++)
-				eu[i] *= factor;
-#endif
-		}
+		for (int t = 0; t < trans_stack.size(); t++)
+			trans_stack[t].specific_rescale();
 	}
 
 	/*
 	 * Set the dimensions of the image.
 	 */
 	void specific_set_dimensions(const image *im) {
-
-		assert(0);
-
-		int new_height = (int) im->height();
-		int new_width  = (int) im->width();
-
-		if (_is_projective) {
-
-			/*
-			 * If P(w, x, y, z) is a projective transform mapping
-			 * the corners of the unit square to points w, x, y, z,
-			 * and Q(w, x, y, z)(i, j) == P(w, x, y, z)(ai, bj),
-			 * then we have:
-			 *
-			 * Q(w, x, y, z) == P(  P(w, x, y, z)(0, 0),
-			 *			P(w, x, y, z)(a, 0),
-			 *			P(w, x, y, z)(a, b),
-			 *			P(w, x, y, z)(0, b)  )
-			 *
-			 * If we note that P(w, x, y, z)(0, 0) == w, we can
-			 * omit a calculation.  
-			 *
-			 * We take 'a' as the ratio (new_height /
-			 * old_height) and 'b' as the ratio (new_width /
-			 * old_width) if we want the common upper left-hand
-			 * region of both new and old images to map to the same
-			 * area.
-			 *
-			 * Since we're not mapping from the unit square, we
-			 * take 'a' as new_height and 'b' as new_width to
-			 * accommodate the existing scale factor.
-			 */
-
-			point _x, _y, _z;
-
-			_x = transform_unscaled(point(new_height,     0    ));
-			_y = transform_unscaled(point(new_height, new_width));
-			_z = transform_unscaled(point(    0     , new_width));
-
-			x[1] = _x;
-			x[2] = _y;
-			x[3] = _z;
-
-		}
+		for (int t = 0; t < trans_stack.size(); t++)
+			trans_stack[t].specific_set_dimensions();
 	} 
 
 	/*
@@ -392,53 +193,7 @@ public:
 	 * file created with an old version of ALE.
 	 */
 	void gpt_v0_set(point x[4]) {
-
-		assert(0);
-
-		_is_projective = 1;
-
-		/*
-		 * This is slightly modified code from version
-		 * 0.4.0p1.
-		 */
-
-		ale_pos delta_01 = x[1][0] - x[2][0];
-		ale_pos delta_02 = x[3][0] - x[2][0];
-		ale_pos sigma_0  = x[0][0] - x[1][0] + x[2][0] - x[3][0];
-		ale_pos delta_11 = x[1][1] - x[2][1];                   
-		ale_pos delta_12 = x[3][1] - x[2][1];                   
-		ale_pos sigma_1  = x[0][1] - x[1][1] + x[2][1] - x[3][1];
-
-		g = (sigma_0  * delta_12 - sigma_1  * delta_02)
-		  / (delta_01 * delta_12 - delta_11 * delta_02)
-		  / (input_width * scale_factor);
-
-		h = (delta_01 * sigma_1  - delta_11 * sigma_0 )
-		  / (delta_01 * delta_12 - delta_11 * delta_02)
-		  / (input_height * scale_factor);
-
-		a = (x[1][0] - x[0][0] + g * x[1][0])
-		  / (input_width * scale_factor);                       
-		b = (x[3][0] - x[0][0] + h * x[3][0])
-		  / (input_height * scale_factor);
-		c = x[0][0];
-
-		d = (x[1][1] - x[0][1] + g * x[1][1])
-		  /  (input_width * scale_factor);                       
-		e = (x[3][1] - x[0][1] + h * x[3][1])
-		  /  (input_height * scale_factor);
-		f =  x[0][1];
-
-		resultant_memo = 1;
-		resultant_inverse_memo = 0;
-
-		this->x[0] = scaled_inverse_transform( point(      0      ,      0      ) );
-		this->x[1] = scaled_inverse_transform( point( (input_height * scale_factor),      0      ) );
-		this->x[2] = scaled_inverse_transform( point( (input_height * scale_factor), (input_width * scale_factor) ) );
-		this->x[3] = scaled_inverse_transform( point(      0      , (input_width * scale_factor) ) );
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
+		trans_stack.top().gpt_v0_set(x);
 	}
 
 	/*
@@ -448,105 +203,13 @@ public:
 	 * file created with an old version of ALE.
 	 */
 	void eu_v0_set(ale_pos eu[3]) {
-
-		assert(0);
-
-		/*
-		 * This is slightly modified code from version
-		 * 0.4.0p1.
-		 */
-
-		int i;
-		
-		x[0][0] = 0;              x[0][1] = 0;
-		x[1][0] = (input_width * scale_factor);    x[1][1] = 0;
-		x[2][0] = (input_width * scale_factor);    x[2][1] = (input_height * scale_factor);
-		x[3][0] = 0;              x[3][1] = (input_height * scale_factor);
-
-		/*
-		 * Rotate
-		 */
-
-		ale_pos theta = eu[2] * M_PI / 180;
-
-		for (i = 0; i < 4; i++) {
-			ale_pos _x[2];
-
-			_x[0] = (x[i][0] - (input_width * scale_factor)/2)  * cos(theta)
-			      + (x[i][1] - (input_height * scale_factor)/2) * sin(theta)
-			      +  (input_width * scale_factor)/2;
-			_x[1] = (x[i][1] - (input_height * scale_factor)/2) * cos(theta)
-			      - (x[i][0] - (input_width * scale_factor)/2)  * sin(theta)
-			      +  (input_height * scale_factor)/2;
-			
-			x[i][0] = _x[0];
-			x[i][1] = _x[1];
-		}
-
-		/*
-		 * Translate
-		 */
-
-		for (i = 0; i < 4; i++) {
-			x[i][0] += eu[0];
-			x[i][1] += eu[1];
-		}
-
-		if (_is_projective) {
-			gpt_v0_set(x);
-			return;
-		}
-
-		/*
-		 * Reconstruct euclidean parameters
-		 */
-			
-		gpt_v0_set(x);
-
-		point center((input_height * scale_factor) / 2, (input_width * scale_factor) / 2);
-		point center_image = transform_scaled(center);
-
-		this->eu[0] = (center_image[0] - center[0]) / scale_factor;
-		this->eu[1] = (center_image[1] - center[1]) / scale_factor;
-
-		point center_left((input_height * scale_factor) / 2, 0);
-		point center_left_image = transform_scaled(center_left);
-
-		ale_pos displacement = center_image[0] - center_left_image[0];
-
-		this->eu[2] = asin(2 * displacement / (input_width * scale_factor)) / M_PI * 180;
-
-		if (center_left_image[1] > center_image[1])
-			this->eu[2] = this->eu[2] + 180;
-
-		resultant_memo = 0;
-		resultant_inverse_memo = 0;
-
-		_is_projective = 0;
-
+		trans_stack.top().eu_v0_set(eu);
 	}
 
 	void debug_output() {
-		assert(0);
-
-		fprintf(stderr, "[t.do ih=%u, iw=%d x=[[%f %f] [%f %f] [%f %f] [%f %f]] eu=[%f %f %f]\n"
-				"      a-f=[%f %f %f %f %f %f %f %f] _a-_f=[%f %f %f %f %f %f %f %f]\n"
-				"      bdcnm=%d ip=%d rm=%d rim=%d sf=%f]\n", 
-				input_height, input_width, 
-				x[0][0], x[0][1], 
-				x[1][0], x[1][1], 
-				x[2][0], x[2][1], 
-				x[3][0], x[3][1], 
-				eu[0], eu[1], eu[2],
-				a, b, c, d, e, f, g, h,
-				_a, _b, _c, _d, _e, _f, _g, _h,
-				bd_count(),
-				_is_projective,
-				resultant_memo,
-				resultant_inverse_memo,
-				scale_factor);
+		for (int t = 0; t < trans_stack.size(); t++)
+			trans_stack[t].debug_output();
 	}
-
 };
 
 #endif
