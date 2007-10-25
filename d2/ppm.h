6864bd1827b6454c101233d1af89c4d04419b51a
David Hilvert <dhilvert@auricle.dyndns.org>
2007-10-25 07:45:00 +0000

    d2/ppm.h: Always scale values in floating point.
    darcs-hash:20071025074548-d1b56-6bbc237871bc8056879cecd2ced073afc8a87e58.gz

--- d2/ppm.h
+++ d2/ppm.h
@@ -273,7 +273,7 @@ static inline image *read_ppm(const char *filename, exposure *e, unsigned int ba
 					error_ppm(filename);
 			}
 
-			p[k] = ((ale_real) ival) / ((ale_real) mcv);
+			p[k] = ((float) ival) / ((float) mcv);
 
 		}
 
@@ -404,11 +404,9 @@ static inline void write_ppm(const char *filename, const image *im, exposure *e,
 			              / (maxval - minval);
 		pixel unlinearized = (e->unlinearize(exposure_adjust)).clamp();
 
-		pixel rescaled = unlinearized * (ale_real) mcv;
-
 		for (k = 0; k < im->depth();  k++) {
 
-			uint16_t output_value = (uint16_t) round(rescaled[k]);
+			uint16_t output_value = (uint16_t) round((float) unlinearized[k] * mcv);
 
 			if (plain) {
 				fprintf(f, "%d ", output_value);
