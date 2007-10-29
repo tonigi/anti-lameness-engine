91c231ce2a9597f2ea2ce8a25872d2ae4197374c
David Hilvert <dhilvert@auricle.dyndns.org>
2007-10-29 03:30:00 +0000

    d2::exposure::exposure_default: Generate a lookup table for linearization and unlinearization operations.
    darcs-hash:20071029033050-d1b56-1411132f941f85951b7ee30a6e4f3b9e4488f661.gz

--- d2/exposure/exposure_default.h
+++ d2/exposure/exposure_default.h
@@ -37,10 +37,117 @@
 class exposure_default : public exposure {
 public:
 	pixel linearize(pixel input) const {
+#if 0
+		/*
+		 * Calling pow() may be expensive on some platforms (e.g.,
+		 * those lacking hardware support for floating point).
+		 */
+
 		return ppow(input, 1/0.45) * get_multiplier();
+#else
+		const int table_size = 1024;
+		const int table_bits = 10;
+		const int interp_bits = 4;
+		static int table_is_built = 0;
+		static ale_real table[table_size];
+
+		pixel result;
+
+		if (!table_is_built) {
+			for (int i = 0; i < table_size; i++) {
+				table[i] = pow((float) i / (float) (table_size - 1), 1/0.45);
+			}
+			table_is_built = 1;
+		}
+
+		for (int k = 0; k < 3; k++) {
+			/*
+			 * Clamp.
+			 */
+			if (input[k] >= 1) {
+				result[k] = 1;
+				continue;
+			} else if (input[k] <= 0) {
+				result[k] = 0;
+				continue;
+			}
+
+			int index1 = ale_real_to_int(input[k], 65535);
+
+			int index2 = index1 >> (16 - table_bits);
+			int index3 = (index1 >> (16 - table_bits - interp_bits)) 
+			           & ((1 << interp_bits) - 1);
+
+			if (index2 >= table_size - 1) {
+				result[k] = 1;
+				continue;
+			}
+
+			ale_real frac = ale_real_from_int((index3 << (16 - interp_bits)), 65535);
+
+			result[k] = (1 - frac) * table[index2] + frac * table[index2];
+		}
+
+		return result * get_multiplier();
+#endif
 	}
 	pixel unlinearize(pixel input) const {
+#if 0
+		/*
+		 * Calling pow() may be expensive on some platforms (e.g.,
+		 * those lacking hardware support for floating point).
+		 */
+
 		return ppow(input / get_multiplier(), 0.45);
+#else
+
+		input /= get_multiplier();
+
+		const int table_size = 1024;
+		const int table_bits = 10;
+		const int interp_bits = 4;
+		static int table_is_built = 0;
+		static ale_real table[table_size];
+
+		pixel result;
+
+		if (!table_is_built) {
+			for (int i = 0; i < table_size; i++) {
+				table[i] = pow((float) i / (float) (table_size - 1), 0.45);
+			}
+			table_is_built = 1;
+		}
+
+		for (int k = 0; k < 3; k++) {
+			/*
+			 * Clamp.
+			 */
+			if (input[k] >= 1) {
+				result[k] = 1;
+				continue;
+			} else if (input[k] <= 0) {
+				result[k] = 0;
+				continue;
+			}
+
+			int index1 = ale_real_to_int(input[k], 65535);
+
+			int index2 = index1 >> (16 - table_bits);
+			int index3 = (index1 >> (16 - table_bits - interp_bits)) 
+			           & ((1 << interp_bits) - 1);
+
+			if (index2 >= table_size - 1) {
+				result[k] = 1;
+				continue;
+			}
+
+			ale_real frac = ale_real_from_int((index3 << (16 - interp_bits)), 65535);
+
+			result[k] = (1 - frac) * table[index2] + frac * table[index2];
+		}
+
+		return result;
+#endif
 	}
 };
 
