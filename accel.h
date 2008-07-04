3eda1b763b7c35c41548a2016e8e9cb1f63d3968
David Hilvert <dhilvert@auricle.dyndns.org>
2008-07-04 01:41:43 +0000

    accel, ale: Add checks for GL features.

--- accel.h
+++ accel.h
@@ -57,15 +57,35 @@ public:
 		if (use_gpu == 2)
 			set_auto();
 
-		if (use_gpu > 0 && gpu::is_ok())
-			return 1;
+		if (use_gpu == 0)
+			return 0;
 
-		if (use_gpu == 1) {
+		if (!gpu::is_ok()) {
 			fprintf(stderr, "GPU acceleration error.\n");
 			exit(1);
 		}
 
-		return 0;	
+		const char *extensions[] = {
+			"GL_ARB_fragment_shader",
+			"GL_ARB_texture_float",
+			"GL_ARB_texture_rectangle",
+			"GL_EXT_framebuffer_object",
+			NULL
+		};
+
+#ifdef USE_GLEW
+		int unsupported = 0;
+		for (const char **c = extensions; *c; c++) {
+			if (!glewIsSupported(*c)) {
+				fprintf(stderr, "GL feature %s not supported.\n", *c);
+				unsupported = 1;
+			}
+		}
+		if (unsupported)
+			exit(1);
+#endif
+
+		return 1;
 	}
 };
 
