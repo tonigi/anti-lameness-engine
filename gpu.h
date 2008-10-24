afe137c5c424441a13b8100b060b3c5b139d7e20
David Hilvert <dhilvert@auricle.dyndns.org>
2008-10-24 14:17:16 +0000

    gpu: Rewrite (define-macro ...) as (ale-define-macro ...).

--- gpu.h
+++ gpu.h
@@ -40,7 +40,7 @@ extern "C" {
 #include "thread.h"
 
 #define ALE_GPU_ASSERT_INCLUDE \
-"(define-macro (assert expr) `(if (not ,expr) (error \"Assertion failed: \" ,(object->string expr))))"
+"(ale-define-macro (assert expr) `(if (not ,expr) (error \"Assertion failed: \" ,(object->string expr))))"
 
 class gpu {
 public:
